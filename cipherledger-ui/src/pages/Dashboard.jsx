f7201162f5fe651f41af3dfce0d18989098cfc13
HET SOLANKI <solankihetpravinbhai2122004@gmail.com>
2026-07-10 21:20:00 +0530

    Add docker deployment

---
+++ cipherledger-ui/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard(){
+
+    return(
+        <div>
+            <h1>CipherLedger Dashboard</h1>
+            <p>Blockchain Explorer Running 🚀</p>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ cipherledger-ui/src/pages/Mining.jsx
@@ -0,0 +1,10 @@
+function Mining(){
+
+    return(
+        <div>
+            <h1>Mining</h1>
+        </div>
+    )
+}
+
+export default Mining;
